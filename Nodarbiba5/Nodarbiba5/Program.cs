1670112e0cfb8c40164808793d4ec9ccc37b0965
Emīls <emils.ribaks@gmail.com>
2018-12-20 19:00:34 +0200

    5. nodarbība ar mājasdarbiem

---
+++ Nodarbiba5/Nodarbiba5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodarbiba5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uzdevumi masivs = new Uzdevumi();
+            masivs.PirmaisMajasdarbs();
+
+            masivs.OtraisMajasdarbs();
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Nodarbiba5/Nodarbiba5/Uzdevumi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodarbiba5
+{
+    class Uzdevumi
+    {
+        public void PirmaisMajasdarbs()
+        {
+            // atrast masīva lielāko skaitli
+            int[] skaitluMasivs = { 1, 2, 3, 7, 9, 11, 19 };
+            int max = skaitluMasivs[0];
+
+            for (int i = 1; i < skaitluMasivs.Length; i++)
+            {
+                if (skaitluMasivs[i] > max)
+                {
+                    max = skaitluMasivs[i];
+                }
+            }
+            Console.WriteLine("Lielākais masīva skaitlis ir " + max);
+        }
+
+        public void OtraisMajasdarbs()
+        {
+            // apgriezt masīvu otrādi
+            int[] skaitluMasivs = { 1, 2, 3, 7, 9, 11, 19 };
+
+            int[] masivsSkaitlu = new int[skaitluMasivs.Length];
+
+            for(int i = 0; i < masivsSkaitlu.Length; i++)
+            {
+                masivsSkaitlu[i] = skaitluMasivs[skaitluMasivs.Length - i - 1];
+                Console.Write(masivsSkaitlu[i] + " ");
+            }
+        }
+
+
+
+    }
+}
