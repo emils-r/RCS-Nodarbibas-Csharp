bf2e421882413e3c301584ff3e21a5931e931f2e
Emīls <emils.ribaks@gmail.com>
2018-12-26 12:21:03 +0200

    Majasdarbs ar dzejoli
    Nolasīt tekstu no dzejolis.txt;
    Apmainīt vietām rindas;
    Saglabāt apmainīto dzejoli jaunā txt failā.

---
+++ MajasdarbsDzejolis/MajasdarbsDzejolis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MajasdarbsDzejolis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] dzejolaRindas = System.IO.File.ReadAllLines(@"D:\Emils\Documents\GitHub\RCS-Nodarbibas-Csharp\MajasdarbsDzejolis\dzejolis.txt");
+            String[] jaunsDzejolis = new string[dzejolaRindas.Length];
+
+            for (int i = 0; i < dzejolaRindas.Length; i++)
+            {
+                jaunsDzejolis[i] = dzejolaRindas[dzejolaRindas.Length - 1 - i];
+                Console.WriteLine(jaunsDzejolis[i]);
+            }
+
+            System.IO.File.WriteAllLines(@"D:\Emils\Documents\GitHub\RCS-Nodarbibas-Csharp\MajasdarbsDzejolis\JaunsDzejolis.txt", jaunsDzejolis);
+            Console.ReadLine();
+        }
+    }
+}
